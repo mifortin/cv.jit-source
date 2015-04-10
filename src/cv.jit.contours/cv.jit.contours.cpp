d6966147635030f672a4d49d575af7bed2c510ea
Michael Fortin <michael.fortin@gmail.com>
2015-04-10 18:52:33 -0400

    First build on jit contours

---
+++ src/cv.jit.contours/cv.jit.contours.cpp
@@ -0,0 +1,240 @@
+/*
+cv.jit.contours
+	
+
+Copyright 2015, Michael Fortin
+
+This file is part of cv.jit.
+
+cv.jit is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published 
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+cv.jit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with cv.jit.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*
+This file links to the OpenCV library <http://sourceforge.net/projects/opencvlibrary/>
+
+Please refer to the  Intel License Agreement For Open Source Computer Vision Library.
+
+Please also read the notes concerning technical issues with using the OpenCV library
+in Jitter externals.
+*/
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "jit.common.h"
+#ifdef __cplusplus 
+} //extern "C"
+#endif
+
+#undef error
+#include "cv.h"
+#include "jitOpenCV.h"
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <memory>
+#include <vector>
+
+
+typedef struct _cv_jit_contours 
+{
+	t_object				ob;
+	float*					data;			//!< Data in the matrix
+	int						numElements;	//!< Elements allocated
+} t_cv_jit_contours;
+
+
+
+void *_cv_jit_contours_class;
+
+t_jit_err 				cv_jit_contours_init(void); 
+t_cv_jit_contours *	cv_jit_contours_new(void);
+void 					cv_jit_contours_free(t_cv_jit_contours *x);
+t_jit_err 				cv_jit_contours_matrix_calc(t_cv_jit_contours *x, void *inputs, void *outputs);
+
+t_jit_err cv_jit_contours_init(void) 
+{
+	long attrflags=0;
+	t_jit_object *mop,*output,*io;
+	
+	_cv_jit_contours_class = jit_class_new("cv_jit_contours",(method)cv_jit_contours_new,(method)cv_jit_contours_free,
+		sizeof(t_cv_jit_contours),A_CANT,0L); //A_CANT = untyped
+
+	//add mop
+	mop = (t_jit_object *)jit_object_new(_jit_sym_jit_mop,1,1);  //Object has one input and one output
+	output = (t_jit_object *)jit_object_method(mop,_jit_sym_getoutput,1); //Get a pointer to the output matrix
+
+   	jit_mop_single_type(mop,_jit_sym_float32);   //Set input type and planecount
+   	jit_mop_single_planecount(mop,1);
+   	
+   	jit_mop_output_nolink(mop,1); //Turn off output linking so that output matrix does not adapt to input
+	
+	//Set input format
+	io = (t_jit_object *)jit_object_method(mop,_jit_sym_getinput,1); //Get a pointer to the input matrix
+	jit_attr_setlong(io,_jit_sym_minplanecount,1);
+  	jit_attr_setlong(io,_jit_sym_maxplanecount,1);
+  	jit_attr_setlong(io,_jit_sym_mindim,2);
+  	jit_attr_setlong(io,_jit_sym_maxdim,2);
+  	jit_attr_setsym(io,_jit_sym_types,_jit_sym_float32);
+   	
+   	jit_attr_setlong(output,_jit_sym_minplanecount,3);  //3 planes:
+  	jit_attr_setlong(output,_jit_sym_maxplanecount,3);	//Blob ID, x-coord, y-coord
+  	jit_attr_setlong(output,_jit_sym_mindim,1); //Only one dimension
+  	jit_attr_setlong(output,_jit_sym_maxdim,1);
+  	jit_attr_setsym(output,_jit_sym_types,_jit_sym_float32); //Coordinates are returned with sub-pixel accuracy
+   	   	
+	jit_class_addadornment(_cv_jit_contours_class,mop);
+	
+	
+	//add methods
+	jit_class_addmethod(_cv_jit_contours_class, (method)cv_jit_contours_matrix_calc, 		"matrix_calc", 		A_CANT, 0L);	
+	
+	//add attributes
+	attrflags = JIT_ATTR_GET_DEFER_LOW | JIT_ATTR_SET_USURP_LOW;
+			
+	jit_class_register(_cv_jit_contours_class);
+
+	return JIT_ERR_NONE;
+}
+
+t_jit_err cv_jit_contours_matrix_calc(t_cv_jit_contours *x, void *inputs, void *outputs)
+{
+	t_jit_err err=JIT_ERR_NONE;
+	long in1_savelock=0, out_savelock=0;
+	t_jit_matrix_info in1_minfo, out_minfo;
+	char *in1_bp;
+	void *in1_matrix, *out_matrix;
+	int match_count;
+	float *in1;
+	CvMat in_mat1;
+	
+	float *matches = 0, *current_match;
+		
+	//Get pointers to matrices
+	in1_matrix 	= jit_object_method(inputs,_jit_sym_getindex,0);
+	out_matrix  = jit_object_method(outputs,_jit_sym_getindex,0);
+
+	if (x&&in1_matrix && out_matrix)
+	{
+		//Lock the matrices
+		in1_savelock = (long) jit_object_method(in1_matrix,_jit_sym_lock,1);
+		out_savelock = (long) jit_object_method(out_matrix,_jit_sym_lock,1);
+		
+		//Make sure input is of proper format
+		jit_object_method(in1_matrix,_jit_sym_getinfo,&in1_minfo);
+		jit_object_method(out_matrix,_jit_sym_getinfo,&out_minfo);
+
+		if(in1_minfo.dimcount != 2)
+		{
+			err = JIT_ERR_MISMATCH_DIM;
+			goto out;
+		}
+		if(in1_minfo.planecount != 1)
+		{
+			err = JIT_ERR_MISMATCH_PLANE;
+			goto out;
+		}
+		if(in1_minfo.type != _jit_sym_float32)
+		{
+			err = JIT_ERR_MISMATCH_TYPE;
+			goto out;
+		}
+		if((in1_minfo.dim[0] != 70)&&(in1_minfo.dim[0] != 134)){
+			err = JIT_ERR_GENERIC;
+			error("Wrong input size, make sure input matrix comes from cv.jit.surf!");
+			goto out;
+		}
+		
+		//Get data
+		jit_object_method(in1_matrix,_jit_sym_getdata,&in1_bp);
+		if(!in1_bp){
+			err = JIT_ERR_INVALID_INPUT;
+			goto out;
+		}
+		
+		cvJitter2CvMat(in1_matrix, &in_mat1);
+		
+		std::vector<std::vector<cv::Point> > contours;
+		std::vector<cv::Vec4i> hierarchy;
+		cv::Mat in_mat1x(&in_mat1);
+		cv::findContours(in_mat1x, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+		
+		//Count the number of elements...
+		int total = 0;
+		for (auto &pointList : contours)
+		{
+			total += pointList.size();
+		}
+		
+		if (total*3 > x->numElements)
+		{
+			delete[] x->data;
+			x->data = new float[total*3];
+			x->numElements = total*3;
+		}
+		
+		// Fill...
+		float *target = x->data;
+		int numContour = 0;
+		for (auto &pointList : contours)
+		{
+			for (cv::Point &pt : pointList)
+			{
+				target[0] = pt.x;
+				target[1] = pt.y;
+				target[2] = numContour;
+				target += 3;
+			}
+			numContour++;
+		}
+		
+		//Prepare output:
+		out_minfo.dim[0] = total;
+		out_minfo.dimcount = 1;
+		out_minfo.planecount = 3;
+		out_minfo.type = _jit_sym_float32;
+		out_minfo.dimstride[0] = 3 * sizeof(float);
+		out_minfo.dimstride[1] = out_minfo.size = out_minfo.dim[0] * out_minfo.dimstride[0];
+		out_minfo.flags = JIT_MATRIX_DATA_REFERENCE | JIT_MATRIX_DATA_FLAGS_USE;
+		jit_object_method(out_matrix,_jit_sym_setinfo_ex, &out_minfo);
+		jit_object_method(out_matrix,_jit_sym_data, x->data);
+	}
+		
+	
+out:
+	jit_object_method(out_matrix,gensym("lock"),out_savelock);
+	jit_object_method(in1_matrix,gensym("lock"),in1_savelock);
+	return err;
+}
+
+
+
+t_cv_jit_contours *cv_jit_contours_new(void)
+{
+	t_cv_jit_contours *x;
+	
+	if ((x=(t_cv_jit_contours *)jit_object_alloc(_cv_jit_contours_class))) {
+		x->data = new float[1000];
+		x->numElements = 1000;
+	} else {
+		x = NULL;
+	}	
+	return x;
+}
+
+void cv_jit_contours_free(t_cv_jit_contours *x)
+{
+	if (x->data) delete[] x->data;
+}
--- src/cv.jit.contours/max.cv.jit.contours.c
+++ src/cv.jit.contours/max.cv.jit.contours.c
@@ -32,18 +32,18 @@ extern "C" {
 } //extern "C"
 #endif
 
-typedef struct _max_cv_jit_surf_match 
+typedef struct _max_cv_jit_contours 
 {
 	t_object		ob;
 	void			*obex;
-} t_max_cv_jit_surf_match;
+} t_max_cv_jit_contours;
 
-t_jit_err cv_jit_surf_match_init(void); 
+t_jit_err cv_jit_contours_init(void); 
 
-void *max_cv_jit_surf_match_new(t_symbol *s, long argc, t_atom *argv);
-void max_cv_jit_surf_match_free(t_max_cv_jit_surf_match *x);
+void *max_cv_jit_contours_new(t_symbol *s, long argc, t_atom *argv);
+void max_cv_jit_contours_free(t_max_cv_jit_contours *x);
 
-void *max_cv_jit_surf_match_class;
+void *max_cv_jit_contours_class;
 
 #ifdef __cplusplus
 extern "C"
@@ -52,18 +52,18 @@ int main(void)
 {	
 	void *p,*q;
 	
-	cv_jit_surf_match_init();
+	cv_jit_contours_init();
 	setup(
-			(t_messlist **)&max_cv_jit_surf_match_class,	//A pointer to the Max class pointer
-			(method)max_cv_jit_surf_match_new,			//The constructor function
-			(method)max_cv_jit_surf_match_free,			//The destructor function
-			(short)sizeof(t_max_cv_jit_surf_match),		//The size of the Max class
+			(t_messlist **)&max_cv_jit_contours_class,	//A pointer to the Max class pointer
+			(method)max_cv_jit_contours_new,			//The constructor function
+			(method)max_cv_jit_contours_free,			//The destructor function
+			(short)sizeof(t_max_cv_jit_contours),		//The size of the Max class
 			0L,											//Use only for GUI objects, null for standard Jitter objects
 			A_GIMME,									//Pass arguments as a list of t_atoms
 			0);											//End of type list
 
-	p = max_jit_classex_setup(calcoffset(t_max_cv_jit_surf_match,obex));	//Setup Max class
-	q = jit_class_findbyname(gensym("cv_jit_surf_match"));				//Get a pointer to the Jitter object class
+	p = max_jit_classex_setup(calcoffset(t_max_cv_jit_contours,obex));	//Setup Max class
+	q = jit_class_findbyname(gensym("cv_jit_contours"));				//Get a pointer to the Jitter object class
     max_jit_classex_mop_wrap(p,q,0); 		
     max_jit_classex_standard_wrap(p,q,0); 	
 
@@ -72,20 +72,20 @@ int main(void)
 	return 0;
 }
 
-void max_cv_jit_surf_match_free(t_max_cv_jit_surf_match *x)
+void max_cv_jit_contours_free(t_max_cv_jit_contours *x)
 {
 	max_jit_mop_free(x);		//Free the matrix operator
 	jit_object_free(max_jit_obex_jitob_get(x));	//Free the Jitter object
 	max_jit_obex_free(x);		//Free the Max wrapper object
 }
 
-void *max_cv_jit_surf_match_new(t_symbol *s, long argc, t_atom *argv)
+void *max_cv_jit_contours_new(t_symbol *s, long argc, t_atom *argv)
 {
-	t_max_cv_jit_surf_match *x;
+	t_max_cv_jit_contours *x;
 	void *o;
 
-	if (x=(t_max_cv_jit_surf_match *)max_jit_obex_new(max_cv_jit_surf_match_class,gensym("cv_jit_surf_match"))) {
-		if (o=jit_object_new(gensym("cv_jit_surf_match"))) {
+	if (x=(t_max_cv_jit_contours *)max_jit_obex_new(max_cv_jit_contours_class,gensym("cv_jit_contours"))) {
+		if (o=jit_object_new(gensym("cv_jit_contours"))) {
 			max_jit_mop_setup_simple(x,o,argc,argv);			
 			max_jit_attr_args(x,argc,argv);
 		} else {
